d6c089e820605d6a1ba0c1aa22861e6dddca9dfa
shubham180295@gmail.com <shubham180295@gmail.com>
2025-01-19 11:38:33 +0530

    files added

--- src/main/java/genericUtility/ExcelUtility.java
+++ src/main/java/genericUtility/ExcelUtility.java
@@ -1,25 +1,32 @@
 package genericUtility;
-
 import java.io.*;
-
+import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
-import pageObjectRepository.ErailHomePage;
-
-import static genericUtility.BaseClass.driver;
 
 public class ExcelUtility {
-
 public  WebDriver driver;
+	public FileInputStream fi;
+	public FileOutputStream fo;
+	public XSSFWorkbook workbook;
+	public XSSFSheet sheet;
+	public XSSFRow row;
+	public XSSFCell cell;
+	public String path;
+	public ExcelUtility(String path) {
+		this. path = path;
+	}
 
-	@Test(priority = 2)
+	@Test
 	public void compareTwoExcelFiles() throws IOException {
 
-		String filePath1 = "DropdownSheet.xlsx";
-		String filePath2 = "StationSheet.xlsx";
+		String filePath1 = "data.xlsx";
+		String filePath2 = "ExpectedStationSheet.xlsx";
 
 		boolean isEqual = compareExcelFiles(filePath1, filePath2);
 		System.out.println("Files are " + (isEqual ? "equal" : "not equal"));
@@ -95,85 +102,69 @@ public  WebDriver driver;
 				return false;
 		}
 	}
-
-
-
-
-	@Test(priority = 1)
+	@Test
 	public void writeExpectedStationIntoExcel() throws IOException {
 		XSSFWorkbook workbook = new XSSFWorkbook();
 		XSSFSheet sheet = workbook.createSheet("expectedStations");
-		sheet.createRow(0).createCell(0).setCellValue("Denduluru");
-		sheet.createRow(1).createCell(0).setCellValue("DEL");
-		sheet.createRow(2).createCell(0).setCellValue("Delang");
-		sheet.createRow(3).createCell(0).setCellValue("DEG");
-		sheet.createRow(4).createCell(0).setCellValue("Delhi");
-		sheet.createRow(5).createCell(0).setCellValue("DLI");
-		sheet.createRow(6).createCell(0).setCellValue("Delhi Azadpur");
-		sheet.createRow(7).createCell(0).setCellValue("DAZ");
-		sheet.createRow(8).createCell(0).setCellValue("Delhi Cantt");
-		sheet.createRow(9).createCell(0).setCellValue("DEC");
-		sheet.createRow(10).createCell(0).setCellValue("Delhi Indrapuri");
-		sheet.createRow(11).createCell(0).setCellValue("DLPI");
-		sheet.createRow(12).createCell(0).setCellValue("Delhi Kishanganj");
-		sheet.createRow(13).createCell(0).setCellValue("DKZ");
-		sheet.createRow(14).createCell(0).setCellValue("Delhi S Rohilla");
-		sheet.createRow(15).createCell(0).setCellValue("DEE");
-		sheet.createRow(16).createCell(0).setCellValue("Delhi Safdarjng");
-		sheet.createRow(17).createCell(0).setCellValue("DSJ");
-		sheet.createRow(18).createCell(0).setCellValue("Delhi Shahdara");
-		sheet.createRow(19).createCell(0).setCellValue("DSA");
-		sheet.createRow(20).createCell(0).setCellValue("Delhi Shahdara A Panel");
-		sheet.createRow(21).createCell(0).setCellValue("DSAP");
-		sheet.createRow(22).createCell(0).setCellValue("Delvada");
-		sheet.createRow(23).createCell(0).setCellValue("DVA");
-		sheet.createRow(24).createCell(0).setCellValue("Adarsh Nagar Delhi");
-		sheet.createRow(25).createCell(0).setCellValue("ANDI");
-		sheet.createRow(26).createCell(0).setCellValue("Bandel Jn");
-		sheet.createRow(27).createCell(0).setCellValue("BDC");
-		sheet.createRow(28).createCell(0).setCellValue("Bodeli");
-		sheet.createRow(29).createCell(0).setCellValue("BDE");
-		sheet.createRow(30).createCell(0).setCellValue("Coromandel");
-		sheet.createRow(31).createCell(0).setCellValue("COL");
-		// Denduluru
-		//DEL
-		//Delang
-		//DEG
-		//Delhi
-		//DLI
-		//Delhi Azadpur
-		//DAZ
-		//Delhi Cantt
-		//DEC
-		//Delhi Indrapuri
-		//DLPI
-		//Delhi Kishanganj
-		//DKZ
-		//Delhi S Rohilla
-		//DEE
-		//Delhi Safdarjng
-		//DSJ
-		//Delhi Shahdara
-		//DSA
-		//Delhi Shahdara A Panel
-		//DSAP
-		//Delvada
-		//DVA
-		//Adarsh Nagar Delhi
-		//ANDI
-		//Bandel Jn
-		//BDC
-		//Bodeli
-		//BDE
-		//Coromandel
-		//COL
+		sheet.createRow(0).createCell(0).setCellValue("Denduluru DEL");
+		sheet.createRow(1).createCell(0).setCellValue("Delang DEG");
+		sheet.createRow(2).createCell(0).setCellValue("Delhi DLI");
+		sheet.createRow(3).createCell(0).setCellValue("Delhi Azadpur DAZ");
+		sheet.createRow(4).createCell(0).setCellValue("Delhi Cantt DEC");
+		sheet.createRow(5).createCell(0).setCellValue("Delhi Indrapuri DLPI");
+		sheet.createRow(6).createCell(0).setCellValue("Delhi Kishanganj DKZ");
+		sheet.createRow(7).createCell(0).setCellValue("Delhi S Rohilla DEE");
+		sheet.createRow(8).createCell(0).setCellValue("Delhi Safdarjng DSJ");
+		sheet.createRow(9).createCell(0).setCellValue("Delhi Shahdara DSA");
+		sheet.createRow(10).createCell(0).setCellValue("Delhi Shahdara A Panel DSAP");
+		sheet.createRow(11).createCell(0).setCellValue("Delvada DVA");
+		sheet.createRow(12).createCell(0).setCellValue("Adarsh Nagar Delhi ANDI");
+		sheet.createRow(13).createCell(0).setCellValue("Bandel Jn BDC");
+		sheet.createRow(14).createCell(0).setCellValue("Bodeli BDE");
+		sheet.createRow(15).createCell(0).setCellValue("Coromandel COL");
+
+
 
 		// create a new excel sheet in current project
-		FileOutputStream fos = new FileOutputStream("StationSheet.xlsx");
+		FileOutputStream fos = new FileOutputStream("ExpectedStationSheet.xlsx");
 		workbook.write(fos);
 		workbook.close();
-
+	}
+	public String readDataFromExcel(String sheetName, int row, int cell) throws
+			EncryptedDocumentException, IOException{
+		FileInputStream fis = new FileInputStream(path);
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+		String data = workbook.getSheet(sheetName).getRow(row).getCell(cell).getStringCellValue();
+        return data;
 	}
 
+	public void setCellData(String sheetName, int rownum, int colnum, String data) throws IOException {
+		File xlfile = new File(path);
+		if (!xlfile.exists())    // If file not exists then create new file
+		{
+			workbook = new XSSFWorkbook();
+			fo = new FileOutputStream(path);
+			workbook.write(fo);
+		}
+
+		fi = new FileInputStream(path);
+		workbook = new XSSFWorkbook(fi);
+
+		if (workbook.getSheetIndex(sheetName) == -1) // If sheet not exists then create new Sheet
+			workbook.createSheet(sheetName);
+		sheet = workbook.getSheet(sheetName);
+
+		if (sheet.getRow(rownum) == null)   // If row not exists then create new Row
+			sheet.createRow(rownum);
+		row = sheet.getRow(rownum);
+
+		cell = row.createCell(colnum);
+		cell.setCellValue(data);
+		fo = new FileOutputStream(path);
+		workbook.write(fo);
+		workbook.close();
+		fi.close();
+		fo.close();
+	}
 
 }
---
+++ src/test/java/comassignment/TC002_UseCase_2.java
@@ -0,0 +1,46 @@
+package comassignment;
+
+import genericUtility.BaseClass;
+import genericUtility.ExtentReportManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pageObjectRepository.OrangeHRM;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+@Listeners(ExtentReportManager.class)
+public class TC002_UseCase_2
+
+{
+    public static WebDriver driver;
+
+    @Test(priority = 1)
+    public void loginWithValidCredentials() throws IOException, InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        OrangeHRM org = new OrangeHRM(driver);
+             org.enterValidDataIntoExcel();
+        driver.quit();
+
+    }
+    @Test(priority = 2)
+    public void loginWithInValidCredentials() throws IOException, InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        OrangeHRM org = new OrangeHRM(driver);
+        org.enterInValidDataIntoExcel();
+        driver.quit();
+
+
+    }
+
+}
